f14980897b8ec703a7d54e6493ae2b87a418d42c
ketunhole <sukorice@gmail.com>
2020-11-11 23:55:25 +0900

    既存のライブラリをコピーしてきた https://github.com/mkaczanowski/BeagleBoneBlack-GPIO/commit/fa5e1c2a2ee3886632090a80f0141881eaa17917 各ピンをインスタンス化したい。

--- src/gpio/gpio.cpp
+++ src/gpio/gpio.cpp
@@ -1,54 +1,10 @@
 /*
- * gpio.cpp
+ * GPIO.cpp
  *
- *  Created on: 2020/11/05
+ *  Created on: 2020/11/11
  *      Author: MunoLike
  */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <poll.h>
-#include <termios.h>
 
-#include "gpio.hpp"
 
-//gpioの有効化関数
-void gpio_export(int n){
-	int fd;
-	char buf[40];
 
-	sprintf(buf, "%d", n);
-
-	fd = open("/sys/class/gpio/export", O_WRONLY);
-	write(fd, buf, strlen(buf));
-	close(fd);
-}
-
-//gpioの有効化解除の関数
-void gpio_unexport(int n){
-	int fd;
-	char buf[40];
-
-	sprintf(buf, "%d", n);
-
-	fd = open("/sys/class/gpio/unexport", O_WRONLY);
-	write(fd, buf, strlen(buf));
-	close(fd);
-}
-
-//gpioの設定ファイルを開く関数
-int gpio_open(int n, char *file, int flag){
-	int fd;
-	char buf[40];
-
-	sprintf(buf, "/sys/class/gpio/gpio%d/%s", n, file);
-
-	fd = open(buf, flag);
-	return fd;
-}
--- src/gpio/gpio.hpp
+++ src/gpio/gpio.hpp
@@ -1,16 +1,13 @@
 /*
- * gpio.hpp
+ * GPIO.hpp
  *
- *  Created on: 2020/11/05
+ *  Created on: 2020/11/11
  *      Author: MunoLike
  */
 
 #ifndef GPIO_GPIO_HPP_
 #define GPIO_GPIO_HPP_
 
-void gpio_export(int n);
-void gpio_unexport(int n);
-int gpio_open(int n, char *file, int flag);
 
 
 
