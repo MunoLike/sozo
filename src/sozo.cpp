b80df5d0486ce4c17e174fecf31e7d6c0f4b782f
haruki0402 <s173129@edu.asahikawa-nct.ac.jp>
2020-11-12 15:37:33 +0900

    超音波センサー

--- src/sozo.cpp
+++ src/sozo.cpp
@@ -1,117 +1,95 @@
-
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
-#define gpio0 60
-#define gpio1 3
-#define gpio2 49
-#define gpio3 115
+#include <poll.h>
+#include <time.h>
 
-int line_sensors[4];
+int distance_front;
 
-int main() {
+//gpioの有効化関数
+void gpio_export(int n) {
+	int fd;
+	char buf[40];
+	sprintf(buf, "%d", n);
+	fd = open("/sys/class/gpio/export", O_WRONLY);
+	write(fd, buf, strlen(buf));
+	close(fd);
+}
+//gpioの有効化解除の関数
+void gpio_unexport(int n) {
+	int fd;
+	char buf[40];
+	sprintf(buf, "%d", n);
+	fd = open("/sys/class/gpio/unexport", O_WRONLY);
+	write(fd, buf, strlen(buf));
+	close(fd);
+}
+//gpioの設定ファイルを開く関数
+int gpio_open(int n, char *file, int flag) {
+	int fd;
+	char buf[40];
+	sprintf(buf, "/sys/class/gpio/gpio%d/%s", n, file);
+	fd = open(buf, flag);
+	return fd;
+}
 
-	FILE *fp,*fp0,*fp1,*fp2,*fp3;
-	int in1=0;
-	char path0[100],path1[100],path2[100],path3[100];
+//パルスのON時間を時刻から算出する関数 単位:micro sec
+//timespec構造体の構造はtv_secに秒,tv_nsecに秒未満の時間をnano秒で表す
 
-		printf("GPIO番号： %d %d %d %d\n", gpio0,gpio1,gpio2,gpio3);
+#define RC_LAP(n, o) ((n.tv_sec - o.tv_sec) * (1000*1000) + (n.tv_nsec - o.tv_nsec) / 1000)
+int main(void)
+{
+	int fd;
+	float Ion;
+	int gpio_number = 27;
+	int ret, i;
+	char c;
+	struct timespec origin;// signal start time
+	struct timespec now;// signal change time
+	struct pollfd pfd;
+	//gpioの有効化
+	gpio_export(gpio_number);
+	//gpioを入力に設定
 
-///////////////////////////////////////////////////////////////////////
-
-		//gpioの有効化
-		fp =fopen("/sys/class/gpio/export","w");
-		fprintf(fp, "%d", gpio0);
-		fclose(fp);
-
-		//gpioを出力い設定
-		sprintf(path0, "/sys/class/gpio/gpio%d/direction",gpio0);
-		fp = fopen(path0,"w");
-		fprintf(fp, "in");
-		fclose(fp);
-
-		//gpio出力用valueファイル名をpathに格納
-		sprintf(path0, "/sys/class/gpio/gpio%d/value",gpio0);
-
-///////////////////////////////////////////////////////////////////////
-
-		fp =fopen("/sys/class/gpio/export","w");
-		fprintf(fp, "%d", gpio1);
-		fclose(fp);
-
-		sprintf(path1, "/sys/class/gpio/gpio%d/direction",gpio1);
-		fp = fopen(path1,"w");
-		fprintf(fp, "in");
-		fclose(fp);
-
-		sprintf(path1, "/sys/class/gpio/gpio%d/value",gpio1);
-
-///////////////////////////////////////////////////////////////////////
-
-		fp =fopen("/sys/class/gpio/export","w");
-		fprintf(fp, "%d", gpio2);
-		fclose(fp);
-
-		sprintf(path2, "/sys/class/gpio/gpio%d/direction",gpio2);
-		fp = fopen(path2,"w");
-		fprintf(fp, "in");
-		fclose(fp);
-
-		sprintf(path2, "/sys/class/gpio/gpio%d/value",gpio2);
-
-////////////////////////////////////////////////////////////////////////
-
-		fp =fopen("/sys/class/gpio/export","w");
-		fprintf(fp, "%d", gpio3);
-		fclose(fp);
-
-		sprintf(path3, "/sys/class/gpio/gpio%d/direction",gpio3);
-		fp = fopen(path3,"w");
-		fprintf(fp, "in");
-		fclose(fp);
-
-		sprintf(path3, "/sys/class/gpio/gpio%d/value",gpio3);
-
-///////////////////////////////////////////////////////////////////////
-
-		printf("%s\n", path0);
-		while(1)
-		{
-
-			fp0 = fopen(path0, "r");
-			fp1 = fopen(path1, "r");
-			fp2 = fopen(path2, "r");
-			fp3 = fopen(path3, "r");
-			//valueファイルを開き，出力値を書き込む
-
-			fscanf(fp0, "%d",&line_sensors[0]);
-
-
-			fscanf(fp1, "%d",&line_sensors[1]);
-
-
-			fscanf(fp2, "%d",&line_sensors[2]);
-
-
-			fscanf(fp3, "%d",&line_sensors[3]);
-
-	        printf("%d %d %d %d\r",line_sensors[0],line_sensors[1],line_sensors[2],line_sensors[3]);
-
-	        fclose(fp0);
-	        fclose(fp1);
-	        fclose(fp2);
-	        fclose(fp3);
+	fd = gpio_open(gpio_number, "direction", O_WRONLY);
+	write(fd, "%d", 2);
+	close(fd);
+	//gpioをedgeに設定
+	fd = gpio_open(gpio_number, "edge", O_WRONLY);
+	write(fd, "both", 4);
+	close(fd);
+	//gpioのvalueファイルを開く
+	fd = gpio_open(gpio_number, "value", O_RDONLY);
+	pfd.fd = fd; //監視するファイルを設定
+	pfd.events = POLLPRI; //監視する通知を設定
+	while (1) { //無条件に繰り返す
+		lseek(fd, 0, SEEK_SET); //読み取り位置を先頭に設定
+		ret = poll(&pfd, 1, -1); //通知を監視
+		read(fd, &c, 1); //通知状態を読み込む
+		if (c == '1') {//パルスの立ち上がりの時刻を取得
+			clock_gettime(CLOCK_MONOTONIC_RAW, &origin);
 		}
+		else continue;
+		lseek(fd, 0, SEEK_SET);
+		ret = poll(&pfd, 1, -1);
+		read(fd, &c, 1);
+		if (c == '0') {//パルスの立ち下がりの時刻を取得
+			clock_gettime(CLOCK_MONOTONIC_RAW, &now);
+		}
+		else continue;
+		Ion = RC_LAP(now, origin);//パルスのON時間を算出
+		distance_front=Ion/61*10;
+		printf("Distance=%d[mm]\n", distance_front);
+		usleep(50000);
+	}
 
-
-		//valueファイルに，出力値0を書き込みGPIOをLOWにする．
-		in1=0;
-		fp = fopen(path1, "w");
-		fprintf(fp, "%d",in1);
-		fclose(fp);
-		usleep(200);
-
-		printf("プログラム終了\n");
-
-		return 0;
+	close(fd);
+	gpio_unexport(gpio_number);
+	return 0;
 }
