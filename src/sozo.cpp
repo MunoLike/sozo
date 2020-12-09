2a4cf0da0ecccda4242ca0998c53848cc6723dab
haruki0402 <s173129@edu.asahikawa-nct.ac.jp>
2020-12-09 18:07:47 +0900

    12月9日

--- src/sozo.cpp
+++ src/sozo.cpp
@@ -1,5 +1,4 @@
 #include <thread>
-#include <chrono>
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
@@ -8,81 +7,99 @@
 #include "supersonic/supersonic.hpp"
 #include "util/console_util.hpp"
 #include "motors/Motor.hpp"
+//#include "soket/client.hpp"
 
 #define PERIOD (10000000)
+Motor right_m("P9_14", "P9_12", "P8_26");
+Motor left_m("P9_22", "P8_16", "P8_18");
 
-void turn_control() {
-  int flag = 0, randam;
-
-  while (flag == 1) {
-    if (line_sensors[0] == 0 || line_sensors[3] == 0) {
-      usleep(100000);
-      flag = 1;
+int turn_left() {
+  left_m.run_pwm(PERIOD, PERIOD * 0.3, DRIVE_MODE::BACKWARD);
+  right_m.run_pwm(PERIOD, PERIOD * 0.3, DRIVE_MODE::FORWARD);
+  while (1) {
+    if (line_sensors[3] == 0) {
+      while (1) {
+        if (line_sensors[3] == 1)
+          while (1) {
+            if (line_sensors[2] == 1) {
+              return 0;
+            }
+          }
+      }
     }
   }
+}
 
-  int turn_flag[3];  //[0]左  [1]中央  [2]右
-  turn_flag[0] = 0;
-  turn_flag[1] = 0;
-  turn_flag[2] = 0;
+int turn_right() {
+  left_m.run_pwm(PERIOD, PERIOD * 0.3, DRIVE_MODE::FORWARD);
+  right_m.run_pwm(PERIOD, PERIOD * 0.3, DRIVE_MODE::BACKWARD);
+  while (1) {
+    if (line_sensors[0] == 0) {
+      while (1) {
+        if (line_sensors[0] == 1)
+          while (1) {
+            if (line_sensors[1] == 1) {
+              return 0;
+            }
+          }
+      }
+    }
+  }
+}
+
+int turn_control() {
+
+  if (line_sensors[0] == 0 || line_sensors[3] == 0) {
+    usleep(10000);
+  } else {
+    return 0;
+  }
 
   if (line_sensors[3] == 0) {
-    turn_flag[0] = 1;
-  }
-  if (line_sensors[0] == 0) {
-    turn_flag[2] = 1;
-  }
-  if (distance_front <= 50) {
-    turn_flag[1] = 1;
-  }
-
-  randam = rand() % 3;
-  flag = 0;
-  while (flag == 1) {
-    if (line_sensors[randam] == 0) {
-      randam = rand() % 3;
-    } else {
-      flag = 1;
-    }
-  }
-
-  flag = 0;
-  while (flag == 1) {
-
+    usleep(100000);
+    turn_left();
+  } else if (line_sensors[0] == 0) {
+    usleep(100000);
+    turn_right();
   }
 }
 
 int main() {
-  Motor left_m("P9_14", "P9_12", "P8_26");
-  Motor right_m("P9_22", "P8_18", "P8_16");
+
+  //実行開始用スイッチ
+  //GPIO::GPIO run_sw("P8_15");
 
   init_linetrace();
   init_supersonic();
 
   std::thread th(read_supersonic);
-
-  sleep(1);
+  sleep(1);  //超音波センサが０を返すので一秒待つ
   while (1) {
     read_linetrace();
     printf("%d %d %d %d %d[mm]\r", line_sensors[0], line_sensors[1],
            line_sensors[2], line_sensors[3], distance_front.load());
 
-    if (distance_front.load() <= 100) {
+    if (distance_front.load() <= 200) {
       left_m.run_pwm(PERIOD, PERIOD * 0, DRIVE_MODE::STOP);
       right_m.run_pwm(PERIOD, PERIOD * 0, DRIVE_MODE::STOP);
       break;
     }
 
-    if (line_sensors[1] == 1) {
-      left_m.run_pwm(PERIOD, PERIOD * 0.2, DRIVE_MODE::FORWARD);
-      right_m.run_pwm(PERIOD, PERIOD * 0.15, DRIVE_MODE::FORWARD);
-    } else if (line_sensors[2] == 1) {
-      left_m.run_pwm(PERIOD, PERIOD * 0.15, DRIVE_MODE::FORWARD);
-      right_m.run_pwm(PERIOD, PERIOD * 0.2, DRIVE_MODE::FORWARD);
-    } else {
-      left_m.run_pwm(PERIOD, PERIOD * 0.2, DRIVE_MODE::FORWARD);
-      right_m.run_pwm(PERIOD, PERIOD * 0.2, DRIVE_MODE::FORWARD);
-    }
+    left_m.run_pwm(PERIOD, PERIOD * 0.4, DRIVE_MODE::FORWARD);
+    right_m.run_pwm(PERIOD, PERIOD * 0.4, DRIVE_MODE::FORWARD);
+
+//    if (line_sensors[1] == 1) {
+//      left_m.run_pwm(PERIOD, PERIOD * 0.3, DRIVE_MODE::FORWARD);
+//      right_m.run_pwm(PERIOD, PERIOD * 0.4, DRIVE_MODE::FORWARD);
+//    } else if (line_sensors[2] == 1) {
+//      left_m.run_pwm(PERIOD, PERIOD * 0.4, DRIVE_MODE::FORWARD);
+//      right_m.run_pwm(PERIOD, PERIOD * 0.3, DRIVE_MODE::FORWARD);
+//    } else {
+//      left_m.run_pwm(PERIOD, PERIOD * 0.4, DRIVE_MODE::FORWARD);
+//      right_m.run_pwm(PERIOD, PERIOD * 0.4, DRIVE_MODE::FORWARD);
+//    }
+
+    //turn_control();
 
     if (utils::kbhit() == 'q') {
       break;
